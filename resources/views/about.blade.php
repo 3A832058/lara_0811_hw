565c5a93211bf5372c344dec24e6fd9ab0cc46e8
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-10-24 05:10:57 +0800

    編寫AboutController和關於本站Route完成

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,7 @@
-<title>首頁</title>
+<title>關於本站</title>
 
 <a href="{{route('home.index')}}">首頁</a><br>
 <a href="{{route('about.index')}}">關於本站</a><br>
 <a href="{{route('home.index')}}">最新消息</a>
+
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('about', [AboutController::class, 'index'])->name('about.index');
